a41437a8201f0d6ac92fa5e66ecebd199871c6fe
Muhammadxon-2oo7 <muhammadxon0709@gmail.com>
2022-09-17 10:52:26 +0500

    4-dars

---
+++ js/app.js
@@ -0,0 +1,408 @@
+//1-misol
+
+// let a = prompt("butun son kiriting:")
+
+// if(a>0){
+//     console.log(true)
+// }else if(a==0){
+//     console.log("musbat ham emas, manfiy ham emas")
+// }else {
+//     console.log(false)
+// }
+
+
+//2-misol
+
+// let a = prompt("son kiriting")
+// if(a%2!=0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//3-misol 
+
+// let a = prompt("son kiriting")
+// if(a%2==0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//4-misol
+
+// let a= prompt("a sonni kiriting")
+// let b= prompt("b sonni kiriting")
+// if(a>0 && b>0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//5-misol
+
+// let a= prompt("a sonni kiriting")
+// let b= prompt("b sonni kiriting")
+// let c= prompt("c sonni kiriting")
+// if(a>b && b>c){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//6-misol
+
+// let a=prompt("a sonni kiriting")
+// let c=prompt("c sonni kiriting")
+// let b=prompt("b sonni kiriting")
+// if(b>c<a){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//7-misol
+
+// let a = prompt("a son:")
+// let b = prompt("b son:")
+
+// if(a%2==0 && b%2==0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//8-misol
+
+// let a=prompt("a sonni kiriting")
+// let b=prompt("b sonni kiriting")
+// let c=prompt("c sonni kiriting")
+// if(a>0 && b>0 && c>0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//9-misol
+
+// let a=prompt("a sonni kiriting")
+// let b=prompt("b sonni kiriting")
+// let c=prompt("c sonni kiriting")
+// if(a>0 && b>0 || c>0 && a>0 ||c>0 && b>0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+//10-misol
+
+// let a=prompt("son kiriting:")
+// const week = ["Dushanba","Seshanba","Chorshanba","Payshanba","Juma","Shanba","Yakshanba"]
+
+// if(a==1){
+//     console.log(week[0])
+// }
+// if(a==2){
+//     console.log(week[1])
+// }
+// if(a==3){
+//     console.log(week[2])
+// }
+// if(a==4){
+//     console.log(week[3])
+// }
+// if(a==5){
+//     console.log(week[4])
+// }
+// if(a==6){
+//     console.log(week[5])
+// }
+// if(a==7){
+//     console.log(week[6])
+// }else{
+//     console.log("bunday hafata kuni yo`q iltimos 1 dan 7gacha bo`lgan sonlarni kiriting")
+// }
+
+//11-misol
+
+// let a = prompt("kod kiriting:")
+// const Kod=["Mobiuz","Uzmobile","Usell","Beeline","Humans"]
+// if(a==88 || a==77 || a==97){
+//     console.log(Kod[0])
+// }
+// if(a==99){
+//     console.log(Kod[1])
+// }
+// if(a==93 || a==94){
+//     console.log(Kod[2])
+// }
+// if(a==90 || a==91){
+//     console.log(Kod[3])
+// }
+// if(a==33){
+//     console.log(Kod[4])
+// }
+
+
+//12-misol
+
+// let a = Number(prompt("son:"))
+// if(a>0){
+//     console.log(a + 1)
+// }
+// else if(a==0){
+//     console.log("ilitmos 0 kiritmang")
+// }else{
+//     console.log(a-1)
+// }
+
+//13-misol
+
+// let a = Number(prompt("son:"))
+// if(a>0){
+//     console.log(a + 3)
+// }
+// else if(a==0){
+//     console.log("ilitmos 0 kiritmang")
+// }else{
+//     console.log(a-2)
+// }
+
+//14-misol
+
+// let a= Number(prompt("a son"))
+// let b= Number(prompt("b son"))
+// if(a>b){
+//     console.log(a +" > " + b)
+// }
+// else if(a==b){
+//     console.log(a + " = " +b)
+// }else{
+//     console.log(a + " < " + b)
+// }
+
+//15-misol
+
+// let a = Number(prompt("a son"))
+// let b = Number(prompt("b son"))
+// let c = Number(prompt("c son"))
+
+// if(a>b && a>c){
+//     console.log("a son katta ")
+// }
+// else if(b>a && b>c){
+//     console.log("b son katta ")
+// }
+// else if(a==b && a==c){
+//     console.log("hamma sonlar teng")
+// }else{
+//     console.log("c son katta")
+// }
+
+//16-misol
+
+// let a = Number(prompt("a son"))
+// let b = Number(prompt("b son"))
+// let c = Number(prompt("c son"))
+
+// if(a<b && a<c){
+//     console.log("a son kichkina ")
+// }
+// else if(b<a && b<c){
+//     console.log("b son kichkina ")
+// }
+// else if(a==b && a==c){
+//     console.log("hamma sonlar teng")
+// }else{
+//     console.log("c son kichkina")
+// }
+
+//17-misol
+
+// let jins =prompt("Erkak , Ayol ikkalasidan birini kiriting");
+// let man = document.querySelector(".man");
+// let woman = document.querySelector(".woman");
+// if(jins == "Erkak" || jins == "erkak" || jins== "1"){
+//     man.style.display="block"
+// }
+// else if(jins =="Ayol" || jins == "ayol" || jins == "2"){
+//     woman.style.display="block"  
+// }
+// else{
+//     console.log("Bunday jins yo`q bizda 😂")
+// }
+
+//18-misol
+
+// let a = prompt("Baho")
+// let Baho=[500 , 400 ,300]
+// if(a == 5){
+//     console.log(Baho[0] + " ming so`m")
+// }
+// else if(a == 4){
+//     console.log(Baho[1] + " ming so`m")
+// }
+// else if( a == 3){
+//     console.log(Baho[3] + " ming so`m")
+// }
+// else{
+//     console.log("🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️")
+// }
+
+//19-misol 
+
+// let a =Number(prompt("son"))
+// if(a>0 && a%2==0){
+//     console.log(a +" bu JUFT musbat son")
+// }
+// else if(a>0 && a%2!=0){
+//     console.log(a +" bu Toq musbat son")
+// }
+// else if(a<0 && a%2==0){
+//     console.log(a +" bu JUFT manfiy son")
+// }
+// else if (a==0){
+//     console.log("musbat ham emas ,manfiy ham emas")
+// }
+// else{
+//     console.log(a +" bu TOQ manfiy son")
+// }
+
+//20-misol
+
+// let yosh = Number(prompt("tug`ulgan yilizni kiriting:"));
+// let userYosh = 2022 - yosh;
+// console.log("Siz " +userYosh + " yoshdasiz")
+
+//21-misol
+
+// let a = Number(prompt("a son"))
+// let b = Number(prompt("b son"))
+// let c = Number(prompt("c son"))
+
+// if(a<=b<=c){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//22-misol
+
+
+// let a = Number(prompt("a son"))
+// let b = Number(prompt("b son"))
+
+// if(a%2==0 && b%2!=0){
+//     console.log("b toq son")
+// }
+// else if(b%2==0 && a%2!=0){
+//     console.log("a toq son")
+// }
+// else if(a%2==0 && b%2==0){
+//     console.log("ikkala son ham juft")
+// }else{
+//     console.log("ikkala son ham toq")
+// }
+
+
+//23-misol 
+
+// let a = Number(prompt("a son"))
+// let b = Number(prompt("b son"))
+// let c = Number(prompt("c son"))
+
+// if(a>0 && b>0 && c>0){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//24-misol
+
+// let a = Number(prompt("a son"))
+// let b = Number(prompt("b son"))
+// let c = Number(prompt("c son"))
+
+// if(a>0 && b<0 && c<0){
+//     console.log("a son musbat")
+// }
+// else if(b>0 && a<0 && c<0){
+//     console.log("b son musbat")
+// }
+// else if(c>0 && b<0 && a<0){
+//     console.log("c son musbat")
+// }
+// else{
+//     alert("Iltimos siz kiritgan sonlardan faqat 1 tasi musbat bo`lsun")
+// }
+
+//25-misol
+
+// let a= Number(prompt("3xonali son kiriting:"))
+// let birinchiRaqami= Math.floor(a/100)
+// let ikkinchiRaqami = -1*((birinchiRaqami*10)-(Math.floor(a/10)))
+// let oxirgiRaqami = a%10
+
+// if(birinchiRaqami != ikkinchiRaqami != oxirgiRaqami){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+//26-misol
+
+// let a= Number(prompt("3xonali son kiriting:"))
+// let b= Math.floor(a/100)
+// let c = -1*((b*10)-(Math.floor(a/10)))
+// let d = a%10
+
+// if(b>c &&  b>d && c>d){
+//     console.log(b+" "+c+" "+d)
+// }
+// else if(b>c &&  b>d && d>c){
+//     console.log(b+" "+d+" "+c)
+// }
+// else if(b<c &&  c>d && b>d){
+//     console.log(c+" "+b+" "+d)
+// }
+// else if(b<c &&  c>d && b<c){
+//     console.log(c+" "+d+" "+b)
+// }
+
+// else if(d>b &&  c<d && b>c){
+//     console.log(d+" "+b+" "+c)
+// }
+// else if(d>b &&  c<d && b<c){
+//     console.log(d+" "+c+" "+b)
+// }
+// else{
+//     console.log("xato")
+// }
+
+
+//27-misol
+
+// let a=prompt("Macbook , Asus , Acer ,Hp , Dell ")
+// let sum=["2000$","1600$","1400$","1200$","1000$"]
+// if(a=="Macbook" || a==1 ||a=="macbook"){
+//     console.log(sum[0])
+// }
+// else if(a=="Asus" || a==2 ||a=="asus"){
+//     console.log(sum[1])
+// }
+// else if(a=="Acer" || a==3 ||a=="acer"){
+//     console.log(sum[2])
+// }
+// else if(a=="Hp" || a==4 ||a=="hp"){
+//     console.log(sum[3])
+// }
+// else if(a=="Dell" || a==5 ||a=="dell" || a=="del"){
+//     console.log(sum[4])
+// }
+// else{
+//     console.log("bizga hali bunday markadagi notebook kelmagan ")
+// }
+
